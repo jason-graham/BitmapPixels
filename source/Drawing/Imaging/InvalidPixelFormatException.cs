7556e9f22df31f1cf7210a56bc22c42954f006da
Jason Graham <jason.graham@csharplabs.com>
2016-11-25 15:31:03 -0800

    Updated file headers

--- source/Drawing/Imaging/InvalidPixelFormatException.cs
+++ source/Drawing/Imaging/InvalidPixelFormatException.cs
@@ -1,6 +1,6 @@
 ﻿//---------------------------------------------------------------------------- 
 //
-//  Copyright (C) CSharp Labs.  All rights reserved.
+//  Copyright (C) Jason Graham.  All rights reserved.
 // 
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
